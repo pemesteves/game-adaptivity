57060d9774374888893b176956661bd02d7aa1ae
Pedro Esteves <up201705160@fe.up.pt>
2022-03-24 11:57:17 +0000

    Separated class into a new file

--- code/firstPrototype/straightSection.js
+++ code/firstPrototype/straightSection.js
@@ -12,27 +12,3 @@ class StraightSection {
         this.Decorate = decorate;
     }
 };
-
-class DecorateSection {
-    constructor() {
-        this.SBegin = this.SEnd = this.EBegin = this.EEnd = 0;
-        this.Rnd1 = this.Rnd2 = this.Rnd3 = this.Rnd4 = [];
-    }
-
-    SetBegin(s, e) {
-        this.SBegin = s;
-        this.EBegin = e;
-    }
-
-    SetEnd(s, e) {
-        this.SEnd = s;
-        this.EEnd = e;
-    }
-
-    SetRandomValues(rnd1, rnd2, rnd3, rnd4) {
-        this.Rnd1.push(rnd1);
-        this.Rnd2.push(rnd2);
-        this.Rnd3.push(rnd3);
-        this.Rnd4.push(rnd4);
-    }
-}
