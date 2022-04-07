2860e51c33eb1dec199e86ca0587ce66301a8498
Pedro Esteves <up201705160@fe.up.pt>
2022-04-07 16:17:51 +0100

    Improved title state

--- code/firstPrototype/predefinedTitleState.js
+++ code/firstPrototype/predefinedTitleState.js
@@ -4,7 +4,35 @@
 **/
 
 class PredefinedTitleState extends TitleState {
+    constructor() {
+        super();
+        
+        this.instructionsFont = null;
+    }
+
     CheckForChange(context) {
         if (Engine.KeyboardInput.IsKeyDown(Engine.Keys.S)) context.ChangeState(new PredefinedLevelState(1, 0));
+    }
+
+    Enter() {
+        super.Enter();
+
+        this.instructionsFont = new Engine.SpriteFont([], Engine.Resources.Images["font"], 6.5, 6.5, SpriteCuts.GetCharArray(0));
+        this.instructionsFont.Strings.push( 
+            { String: "Arrows: Move", X: 1, Y: 5 },
+            { String: "A: Move Faster/Shoot", X: 1, Y: 15 },
+            { String: "S: Jump", X: 1, Y: 25 },
+        );
+    }
+
+    Exit() {
+        super.Exit();
+        delete this.instructionsFont;
+    }
+
+    Draw(context) {
+        super.Draw(context);
+        
+        this.instructionsFont.Draw(context, this.Camera);
     }
 };
